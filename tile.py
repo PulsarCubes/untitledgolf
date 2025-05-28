bd8cd4418b15910627fb4bd0335a52092cd025c4
PulsarCubes <115654164+PulsarCubes@users.noreply.github.com>
2025-05-27 19:23:52 -0500

    first few tile types
    cannot believe this took me 40 minutes

--- tile.py
+++ tile.py
@@ -3,18 +3,52 @@ import wall
 
 
 class Tile(pg.sprite.Sprite):
-    def __init__(self, type, center=(0, 0)):
+    def __init__(self, type, orientation=0, center=(0, 0)):
         super().__init__()
         self.image = pg.Surface((200, 200))
         self.image.fill((68, 234, 82))
         self.rect = self.image.get_rect()
+        self.orientation = orientation
+        #note that center is not using pygame coords its using a seperate system
         self.center = center
         self.type = type
         self.wallgroup = pg.sprite.Group()
+
         types = {"hallway": 2, "slanted_hallway": 2, "open": 3, "single": 1, "empty": 0, "triangle": 1}
         for i in range(types[type]):
             _wall = wall.Wall(200)
             self.wallgroup.add(_wall)
+        wall_list = list(self.wallgroup)
+        if self.type == "hallway":
+            if orientation % 2 == 0:
+                wall_list[1].rect.topleft = (190, 0)
+            else:
+                for _wall in wall_list:
+                    _wall.image = pg.transform.rotate(_wall.image, self.orientation*90)
+                    _wall.rect = _wall.image.get_rect(center=_wall.rect.center)
+                    wall_list[1].rect.topleft = (0, 190)
+            wall_list[0].rect.topleft = (0, 0)
+        if self.type == "single":
+            if orientation % 2 == 0:
+                wall_list[0].rect.topleft = orientation*95,0
+            else:
+                wall_list[0].image = pg.transform.rotate(wall_list[0].image, self.orientation*90)
+                wall_list[0].rect.topleft = (0, (orientation-1)*95)
+        if self.type == "open":
+            if orientation % 2 == 0:
+                
+                wall_list[1].rect.topleft = (190, 0)
+                wall_list[2].image = pg.transform.rotate(wall_list[2].image, (self.orientation+1)*90)
+                wall_list[2].rect = wall_list[2].image.get_rect(center=wall_list[2].rect.center)
+                wall_list[2].rect.topleft = (0, orientation*95)
+            else:
+                for _wall in wall_list[:2]:
+                    _wall.image = pg.transform.rotate(_wall.image, self.orientation*90)
+                    _wall.rect = _wall.image.get_rect(center=_wall.rect.center)
+                    wall_list[1].rect.topleft = (0, 190)
+                wall_list[2].rect.topleft = ((orientation-1)*95,0)
+            wall_list[0].rect.topleft = (0, 0)
+
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
--- wall.py
+++ wall.py
@@ -4,7 +4,8 @@ class Wall(pg.sprite.Sprite):
     def __init__(self,length,x=0, y=0):
         super().__init__()
         self.length = length
-        self.image = pg.Surface((length,10))
+        self.image = pg.Surface((15,length))
+        self.image.fill((250, 249, 246))
         self.rect = self.image.get_rect()
         self.x = self.rect.x
         self.y = self.rect.y
