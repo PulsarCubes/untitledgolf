f59c73b6fd14570141a0b64f373582c0ab79c8ce
PulsarCubes <115654164+PulsarCubes@users.noreply.github.com>
2025-09-13 22:00:32 -0500

    finish off tile class i hope
    done for now i will eventually lock in and make rudimentary procedural gen

--- tile.py
+++ tile.py
@@ -3,14 +3,16 @@ import wall
 
 
 class Tile(pg.sprite.Sprite):
-    def __init__(self, type, orientation=0, center=(0, 0)):
+    def __init__(self, type, orientation=0, grid_coord=(0, 0)):
         super().__init__()
+        
         self.image = pg.Surface((200, 200))
-        self.image.fill((68, 234, 82))
+        if type != "triangle":
+            self.image.fill((68, 234, 82))
         self.rect = self.image.get_rect()
         self.orientation = orientation
-        #note that center is not using pygame coords its using a seperate system
-        self.center = center
+        self.grid_coord = grid_coord
+        self.rect.topleft = (grid_coord[0] * 200, grid_coord[1] * 200)
         self.type = type
         self.open_faces = []
         self.wallgroup = pg.sprite.Group()
@@ -22,8 +24,10 @@ class Tile(pg.sprite.Sprite):
         wall_list = list(self.wallgroup)
         if self.type == "hallway":
             if orientation % 2 == 0:
+                self.open_faces = [0, 2]
                 wall_list[1].rect.topleft = (190, 0)
             else:
+                self.open_faces = [1, 3]
                 for _wall in wall_list:
                     _wall.image = pg.transform.rotate(_wall.image, self.orientation * 90)
                     _wall.rect = _wall.image.get_rect(center=_wall.rect.center)
@@ -37,6 +41,7 @@ class Tile(pg.sprite.Sprite):
                 wall_list[0].image = pg.transform.rotate(wall_list[0].image, self.orientation * 90)
                 wall_list[0].rect.topleft = (0, (orientation - 1) * 95)
         if self.type == "open":
+            self.open_faces.append(orientation)
             if orientation % 2 == 0:
 
                 wall_list[1].rect.topleft = (190, 0)
@@ -51,6 +56,10 @@ class Tile(pg.sprite.Sprite):
                 wall_list[2].rect.topleft = ((orientation - 1) * 95, 0) 
             wall_list[0].rect.topleft = (0, 0)
         if self.type == "triangle":
+            if orientation == 0:
+                open_faces = [0,4]
+            else:
+                open_faces = [orientation, orientation-1]
             wall_list[0].image = pg.Surface((int(200*1.4),15), pg.SRCALPHA)
             wall_list[0].image.fill(((250, 249, 246)))
             wall_list[0].image = pg.transform.rotate(wall_list[0].image,45)
@@ -59,10 +68,14 @@ class Tile(pg.sprite.Sprite):
             if orientation % 2 == 0:
                 wall_list[0].image = pg.transform.rotate(wall_list[0].image, 90)
                 wall_list[0].rect = wall_list[0].image.get_rect()
-                wall_list[0].topleft = 0,0
+                wall_list[0].rect.topleft = (0,0)
+                pg.draw.polygon(self.image, (68, 234, 82), [(0,0), (orientation*100,(2-orientation)*100), (200, 200)])
+            else:
+                pg.draw.polygon(self.image, (68, 234, 82), [(200,0), ((orientation-1)*100,(orientation-1)*100), (0, 200)])
         if self.type == "slanted_hallway":
+            #this is gonna be AWFUL to handle im realizing lmao 
+            open_faces = [0,2]
             self.image = pg.Surface((200, 400))
-            pg.draw.polygon(self.image, (68, 234, 82), [(0, 200), (200, 0), (200, 200), (0, 400)])
             if orientation % 2 == 0:
                 self.image = pg.transform.flip(self.image, True, False)
             for _wall in wall_list:
@@ -73,6 +86,12 @@ class Tile(pg.sprite.Sprite):
                 _wall.rect.center = (100, 100)
             wall_list[0].rect.topleft = (0, 0)
             wall_list[1].rect.topright = (200,200)
+        if self.type != "empty":
+            for _wall in wall_list:
+                _wall.rect.x+=self.rect.x
+                _wall.rect.y+=self.rect.y
+        else:
+            self.open_faces = [0, 1, 2, 3]
     def draw(self, surface):
         surface.blit(self.image, self.rect)
         self.wallgroup.draw(surface)
